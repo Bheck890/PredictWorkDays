c177e4ab2b0053b9c841e86c053225d94042b012
Brandon Heck <bheck756@gmail.com>
2024-09-14 20:39:34 -0400

    Program Upload

---
+++ DatesWork/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace DatesWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePickerBegin = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownPay = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDownWorkWeek = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWorkWeek)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePickerBegin
+            // 
+            this.dateTimePickerBegin.Location = new System.Drawing.Point(131, 25);
+            this.dateTimePickerBegin.Name = "dateTimePickerBegin";
+            this.dateTimePickerBegin.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerBegin.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Begin";
+            // 
+            // numericUpDownPay
+            // 
+            this.numericUpDownPay.Location = new System.Drawing.Point(131, 113);
+            this.numericUpDownPay.Maximum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numericUpDownPay.Minimum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownPay.Name = "numericUpDownPay";
+            this.numericUpDownPay.Size = new System.Drawing.Size(68, 20);
+            this.numericUpDownPay.TabIndex = 8;
+            this.numericUpDownPay.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Days After End";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Week Length";
+            // 
+            // numericUpDownWorkWeek
+            // 
+            this.numericUpDownWorkWeek.Location = new System.Drawing.Point(131, 77);
+            this.numericUpDownWorkWeek.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numericUpDownWorkWeek.Minimum = new decimal(new int[] {
+            7,
+            0,
+            0,
+            0});
+            this.numericUpDownWorkWeek.Name = "numericUpDownWorkWeek";
+            this.numericUpDownWorkWeek.Size = new System.Drawing.Size(68, 20);
+            this.numericUpDownWorkWeek.TabIndex = 10;
+            this.numericUpDownWorkWeek.Value = new decimal(new int[] {
+            13,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(357, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(339, 449);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(36, 157);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(147, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Add Payday Month Count";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 479);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDownWorkWeek);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDownPay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePickerBegin);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Work Week Pay Period";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWorkWeek)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerBegin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownPay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDownWorkWeek;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ DatesWork/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatesWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> NewDate = new List<string>();
+        List<string> CountDates = new List<string>();
+        DateTime Begin;
+        DateTime PayDay;
+
+        int CurrMonth;
+        int CountMonth;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set the Multiline property to true.
+            textBox1.Multiline = true;
+            // Add vertical scroll bars to the TextBox control.
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            // Allow the TAB key to be entered in the TextBox control.
+            textBox1.AcceptsReturn = true;
+            // Allow the TAB key to be entered in the TextBox control.
+            textBox1.AcceptsTab = true;
+            // Set WordWrap to true to allow text to wrap to the next line.
+            textBox1.WordWrap = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            //Calculate the Next Weeks
+            DateTime Start = dateTimePickerBegin.Value;
+
+            Begin = Start;
+
+            Console.WriteLine($"Start: {Start.Month}-{Start.Day}-{Start.Year}");
+
+            CurrMonth = Begin.Month;
+
+            while (Begin.Year <= Start.Year)
+            {
+                if (PayDay.Month == CurrMonth)
+                {
+                    CountMonth++;
+                }
+                else
+                {
+                    if (checkBox1.Checked && CountMonth == 3)
+                        CountDates.Add($"3 Paydays for Month: {CurrMonth}");
+
+                    CurrMonth = PayDay.Month;
+                    CountMonth = 1;
+                }
+
+                NewDate.Add(NextWeek());
+            }
+
+            //Extra 3
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    NewDate.Add(NextWeek());
+
+            //}
+
+            foreach (var item in NewDate)
+            {
+                textBox1.Text += item + "\r\n";
+            }
+            foreach (var item in CountDates)
+            {
+                textBox1.Text += item + "\r\n";
+            }
+
+            textBox1.Text += $"Work Weeks: {NewDate.Count}" + "\r\n";
+
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.ScrollToCaret();
+
+            NewDate.Clear();
+            CountDates.Clear();
+        }
+
+        string NextWeek()
+        {
+            DateTime EndDay = Begin.AddDays((int)numericUpDownWorkWeek.Value);//13
+
+            PayDay = EndDay.AddDays((int)numericUpDownPay.Value); //4
+
+            string outDays = $"{Begin.Month}/{Begin.Day}-{EndDay.Month}/{EndDay.Day} R:{PayDay.Month}/{PayDay.Day} ";
+
+            Begin = EndDay.AddDays(1);
+            Console.WriteLine(outDays);
+
+            return outDays;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
